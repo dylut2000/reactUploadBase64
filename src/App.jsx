46a0560a478c6188d09ade23d19597a825b0e9d8
Hardy Lutula <hardylutula@gmail.com>
2023-11-29 08:21:44 +0200

    this is the simplest way to upload a base64.. you can easily fit it into any app at any level

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [file, setFile] = useState(null)
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0]
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      const base64String = reader.result
+      console.log(base64String)
+      setFile(base64String)
+    }
+
+    reader.readAsDataURL(selectedFile)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <input type='file' onChange={handleFileChange} />
+      {file && <img src={file} alt='Uploaded file' />}
+    </div>
   )
 }
 
